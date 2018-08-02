ab2ba1a0bf3ae4e0e4852bd68e277948f65a45f1
mabing <cumt_ttr@163.com>
2018-08-02 22:54:39 +0800

    增加2个单链表相加

--- python/SingleChain.py
+++ python/SingleChain.py
@@ -115,6 +115,26 @@ class SingleChain(object):
             prev = current
         self.head = prev
 
+    def __add__(self, other):
+        """
+        两个单链表相加, 1->2->3 + 4->5 = 168
+        :param other:
+        :return:
+        """
+        reversed(self)
+        reversed(other)
+        result = []
+        for item in [self, other]:
+            cursor = item.head
+            count = 0
+            add_result = 0
+            while cursor is not None:
+                add_result += cursor.data * (10 ** count)
+                count += 1
+                cursor = cursor.next
+            result.append(add_result)
+        return sum(result)
+
 
 if __name__ == "__main__":
     s = SingleChain()
@@ -123,15 +143,28 @@ if __name__ == "__main__":
     s.append(3)
     print s.size
     print s.show()
+    print "取元素", "=" * 15
     print s[0].data
     print s[1].data
     print s[2].data
+    print "设置元素", "=" * 15
     s[0] = -1
     s[1] = 11
     s[4] = 4
+    print "反向", "=" * 15
     print s.show()
     reversed(s)
     print s.show()
-    print "="*15
+    print "迭代", "=" * 15
     for item in s:
         print item.data
+
+    print "相加", "=" * 15
+    s1 = SingleChain()
+    s1.append(1)
+    s1.append(2)
+    s1.append(3)
+    s2 = SingleChain()
+    s2.append(4)
+    s2.append(5)
+    print s2 + s1
