7c012369246575a5aee2965b24076ba101aa8690
mabing <cumt_ttr@163.com>
2018-08-09 17:24:58 +0800

    完善Python版的二叉树

--- python/tree/BinaryTree.py
+++ python/tree/BinaryTree.py
@@ -37,6 +37,55 @@ class Tree(object):
     def get_val(self):
         return self.val
 
+    def preorder_traversal(self):
+        """
+        前序遍历算法
+        :return:
+        """
+        print self.get_val()
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
+
+    def postorder_traversal(self):
+        """
+        后序遍历算法
+        :return:
+        """
+        if self.left:
+            self.left.postorder_traversal()
+        if self.right:
+            self.right.postorder_traversal()
+        print self.get_val()
+
+    def inorder_traversal(self):
+        """
+        中序遍历算法
+        :return:
+        """
+        if self.left:
+            self.left.inorder_traversal()
+        print self.get_val()
+        if self.right:
+            self.right.inorder_traversal()
+
+    def breadth_first_traversal(self):
+        """
+            宽度优先遍历
+            参考：https://codereview.stackexchange.com/questions/191984/perform-bfs-on-a-binary-tree
+        """
+
+        # root = self.root if root is None else root
+        to_visit = [self]
+        while to_visit:
+            current = to_visit.pop(0)
+            print(current.val)
+            if current.left:
+                to_visit.append(current.left)
+            if current.right:
+                to_visit.append(current.right)
+
 
 if __name__ == "__main__":
     #
@@ -45,9 +94,7 @@ if __name__ == "__main__":
     #                 b    c
     #                /\    /\
     #               d  e  f  g
-    #
-    #
-    #
+
     t = Tree('a')
     t.insert_left('b')
     t.insert_right('c')
@@ -55,7 +102,7 @@ if __name__ == "__main__":
     t.get_left().insert_right('e')
     t.get_right().insert_left('f')
     t.get_right().insert_right('g')
-    print "打印============================"
+    print "手动'宽度优先'搜索打印============"
     print t.get_val()  # a
     print t.get_left().get_val()  # b
     print t.get_right().get_val()  # c
@@ -63,3 +110,13 @@ if __name__ == "__main__":
     print t.get_left().get_right().get_val()  # e
     print t.get_right().get_left().get_val()  # f
     print t.get_right().get_right().get_val()  # g
+    print "前序遍历========================"
+    t.preorder_traversal()
+    print "后序遍历========================"
+    t.postorder_traversal()
+    print "中序遍历========================"
+    t.inorder_traversal()
+    print "'宽度优先搜索'算法打印==========="
+    t.breadth_first_traversal()
+
+
