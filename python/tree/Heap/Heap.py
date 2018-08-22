3473b640002e64143cd11599e30f45816d55d49f
mabing <cumt_ttr@163.com>
2018-08-22 17:16:19 +0800

    增加Python版最大堆的注释和完善

--- python/tree/Heap/Heap.py
+++ python/tree/Heap/Heap.py
@@ -1,75 +1,111 @@
 # encoding: utf-8
-#!/usr/bin/python
 """
     来源: https://github.com/TheAlgorithms/Python/blob/master/data_structures/Heap/heap.py
     关于堆的解释: https://blog.csdn.net/juanqinyang/article/details/51418629
     python自带的heap模块: https://blog.csdn.net/minxihou/article/details/51857518
-    卧槽,还不如看python内置的heapq模块,就特么一个文件,加注释才485行
+    堆的排序: https://www.cnblogs.com/chengxiao/p/6129630.html
+    =======================================================
+    http://bubkoo.com/2014/01/14/sort-algorithm/heap-sort/
+    几个计算公式：
+
+        Parent(i) = floor((i-1)/2)，i 的父节点下标
+        Left(i) = 2i + 1，i 的左子节点下标
+        Right(i) = 2(i + 1)，i 的右子节点下标
+
 """
-from __future__ import print_function
-
-try:
-    raw_input  # Python 2
-except NameError:
-    raw_input = input  # Python 3
 
 
-class Heap:
+class Heap(object):
     def __init__(self):
         self.h = []
         self.currsize = 0
 
-    def leftChild(self, i):
+    def left_child(self, i):
+        """
+        获取左边节点
+        :param i:
+        :return:
+        """
         if 2 * i + 1 < self.currsize:
             return 2 * i + 1
         return None
 
-    def rightChild(self, i):
+    def right_child(self, i):
+        """
+        获取右边节点
+        :param i:
+        :return:
+        """
         if 2 * i + 2 < self.currsize:
             return 2 * i + 2
         return None
 
-    def maxHeapify(self, node):
+    def max_heapify(self, node):
+        """
+        构建最大堆
+        :param node:
+        :return:
+        """
+        print ("---------------------------------------")
+        print("node(old) = %s, self.h = %s" % (node, self.h))
         if node < self.currsize:
             m = node
-            lc = self.leftChild(node)
-            rc = self.rightChild(node)
+            lc = self.left_child(node)
+            rc = self.right_child(node)
+            if lc is not None:
+                print ("self.h[%s] > self.h[%s]" % (lc, m), self.h[lc] > self.h[m])
             if lc is not None and self.h[lc] > self.h[m]:
                 m = lc
+            if rc is not None:
+                print("self.h[%s] > self.h[%s], " % (rc, m), self.h[rc] > self.h[m])
             if rc is not None and self.h[rc] > self.h[m]:
                 m = rc
             if m != node:
                 temp = self.h[node]
                 self.h[node] = self.h[m]
                 self.h[m] = temp
-                self.maxHeapify(m)
+                print("m(new) = %s, node(old) = %s, self.h = %s" % (m, node, self.h))
+                print("===========================================")
+                self.max_heapify(m)
 
-    def buildHeap(self, a):
+    def build_heap(self, a):
+        """
+        构建堆
+        :param a:
+        :return:
+        """
         self.currsize = len(a)
         self.h = list(a)
-        print("self.currsize = ", self.currsize)
         for i in range(self.currsize / 2, -1, -1):
-            self.maxHeapify(i)
+            self.max_heapify(i)
 
-    def getMax(self):
+    def get_max(self):
+        """
+        获得最大值
+        :return:
+        """
         if self.currsize >= 1:
             me = self.h[0]
             temp = self.h[0]
             self.h[0] = self.h[self.currsize - 1]
             self.h[self.currsize - 1] = temp
             self.currsize -= 1
-            self.maxHeapify(0)
+            self.max_heapify(0)
             return me
         return None
 
-    def heapSort(self):
+    def heap_sort(self):
+        """
+        堆排序
+        :return:
+        """
         size = self.currsize
         while self.currsize - 1 >= 0:
             temp = self.h[0]
             self.h[0] = self.h[self.currsize - 1]
             self.h[self.currsize - 1] = temp
             self.currsize -= 1
-            self.maxHeapify(0)
+            self.max_heapify(0)
         self.currsize = size
 
     def insert(self, data):
@@ -87,12 +123,39 @@ class Heap:
 
 
 def main():
-    # l = list(map(int, raw_input().split()))
-    l = [1,10,25,30,9]
-    print(l)
+    """
+                5
+               / \
+              /   \
+             4     6
+            / \   /\
+           /   \ /  \
+          3    7 2   8
+         /
+        /
+       1
+    :return:
+    """
+    l = [5, 4, 6, 3, 7, 2, 8, 1]
     h = Heap()
-    h.buildHeap(l)
-    h.heapSort()
+    h.build_heap(l)
+    print "after build,l= "
+    h.display()
+    """
+        每个子树的顶端值都大于其叶子节点的值, 最大堆
+                8
+               / \
+              /   \
+             7     6
+            /  \   /\
+           /    \ /  \
+          3     4 2   5
+         /
+        /
+       1
+    """
+    h.heap_sort()
+    print "after sort,l= "
     h.display()
 
 
